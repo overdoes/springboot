f3291f67fbcf9409d76cb6723794b8da9e1df5da
lizhijie <13011091536@163.com>
2020-11-26 20:10:01 +0800

    add  注释

--- gitdemo/src/main/java/com/example/gitttt/gitdemo/GitdemoApplication.java
+++ gitdemo/src/main/java/com/example/gitttt/gitdemo/GitdemoApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitdemoApplication {
 
+	/**
+	 * init
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(GitdemoApplication.class, args);
 	}
